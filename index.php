9353b01163aa70e695c6bb9d487ba261e34b0c8e
jea2-prog <jjohnayo17@gmail.com>
2022-01-18 21:56:18 -0800

    Php basics

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php 
+  //$stringone ='My email is: ';
+  //$stringtwo = 'johnayo17@gmail.com';
+  //echo $stringone.$stringtwo;
+  $name = 'John';
+  $radius = 25;
+  $pi = 3.14;
+  $age = 20;
+  //increment & decrement
+   //$radius--;
+   //echo $radius;
+ //$age  += 10;
+ //echo $age;
+ //echo floor($pi);
+ //echo ceil($pi);
+ //echo pi();  
+ //Array
+ //$peopleone = ['John','Esther','Joy'];
+ //echo $peopleone[1];
+
+ //$peopletwo = array('James','Jane');
+ //echo $peopletwo[1];
+ $ages=[10,14,19];
+ print_r ($ages);
+?>
+
+
+<html>
+    <head>
+        <title>PHP Tutorial</title>
+    </head>
+    <p>
+        <div>
+          <?php 
+          //echo "My name is ".$name;
+            //echo 'nigga "wahoo"';          
+          ?>
+        </div>
+        <div>
+            <?php
+              //echo "the nigga \"whaoo \""
+            ?>
+        </div>
+        <div>
+            <?php
+              //echo $name[1];
+            ?>
+        </div>
+        <div>
+            <?php
+            //echo strlen($name);
+            //echo strtoupper($name);
+           // echo strtolower($name);
+           //echo str_replace('J','A',$name);
+           //*Numbers */
+           //echo   $pi * $radius**2 ;
+          // echo '---------------';
+           //echo $pi + $radius;
+           //echo '---------------';
+           //echo 2*(4+9)/3;
+           
+            ?>
+        </div>
+    </p>
+</html>
---
+++ index_1.php
@@ -0,0 +1,33 @@
+<?php
+define('NAME','Jesuyon');
+//$name ='John';
+$age = 30;
+//$name ='Olugbem';
+
+?>
+
+
+
+ 
+<html>
+    <head>
+         <title>
+           PHP Tutorial
+         </title>           
+            
+        
+    </head>
+    <body>
+        
+        <h1>
+            User Profile Page
+            
+        </h1>
+        <div>
+            <?php echo NAME; ?>
+        </div>
+        <div>
+            <?php echo $age ?>
+        </div>
+    </body>
+</html>
